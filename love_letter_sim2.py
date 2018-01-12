cd1646ef237ac04faaf2304dcd85854e90080417
amosmj <amosmj@gmail.com>
2018-01-11 19:13:04 -0600

    Add files via upload

---
+++ love_letter_sim2.py
@@ -0,0 +1,11 @@
+
+
+class deck:
+    def __init__(self):
+        self.names = ["Guard"]
+        self.power = range(1,8)
+        self.qty.  = [5,3]
+        self.game_deck = []
+
+class player:
+    def __init__(self):
